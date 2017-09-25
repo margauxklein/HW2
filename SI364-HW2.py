502128ea47b7971be3aab4810ac1f99932bc563d
Margaux <margauxk@umich.edu>
2017-09-25 19:44:19 -0400

    Final commit

--- SI364-HW2.py
+++ SI364-HW2.py
@@ -9,8 +9,9 @@
 
 ## Edit the following Flask application code so that if you run the application locally and got to the URL http://localhost:5000/question, you see a form that asks you to enter your favorite number. Once you enter a number and submit it to the form, you should then see a web page that says "Double your favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says "Double your favorite number is 4". Careful about types in your Python code!
 ## You can assume a user will always enter a number only.
-
-from flask import Flask
+import requests
+import json
+from flask import Flask, request
 app = Flask(__name__)
 app.debug = True
 
@@ -18,7 +19,68 @@ app.debug = True
 def hello_to_you():
     return 'Hello!'
 
+@app.route('/question', methods= ['POST','GET'])
+def enter_data():
+    s = """<!DOCTYPE html>
+	<html>
+	<body>
 
+		<form action="http://localhost:5000/result" method="GET">
+		<div>
+		  Enter your favorite number: <b>
+		    <input type = "text" name="number" value="">
+		    <br>
+		    <input type="submit" value= "Submit">
+		 <div>
+		</form> 
+
+	</body>
+	</html>""" 
+    return s
+
+@app.route('/result', methods = ['POST', 'GET'])
+def double():
+    if request.method == 'GET':
+        result = request.args
+        favorite_num = result.get('number')
+        favorite_num_2 = 2*(int(favorite_num))
+        return "Double your favorite number is {}".format(favorite_num_2)
+
+@app.route('/question2', methods = ['GET'])
+def enter_movie():
+	s = """<!DOCTYPE html>
+		<html>
+		<body>	
+	<form name="myForm" action="/omdb_data" method="get">
+	<p>
+	Enter your favorite movie: <b> <br>
+	<input type="radio" name="options" value="Cinderella"> Cinderella <br>
+	<input type="radio" name="options" value="Hunger Games"> Hunger Games <br>
+	<input type="radio" name="options" value="Forrest Gump"> Forrest Gump <br>
+	</p>
+	<p><input type=submit value=Next></p>
+	</form>
+			</form> 
+		</body>
+		</html>""" 
+	return s
+
+@app.route('/omdb_data', methods = ['GET'])
+def get_omdb_data():
+	result = request.args
+	favorite_mov= result.get('options')
+	#print (favorite_mov)
+	base_url = "https://itunes.apple.com/search?term=" 
+	url = base_url + favorite_mov
+	x = requests.get(url, params = {"media": "movie"}).text
+	return json.loads(x)["results"][0]["longDescription"]
+
+
+
+# 	base_url = "https://itunes.apple.com/search?term=" 
+# 	url = base_url + mov
+# 	x = requests.get(url).text
+# 	return x
 if __name__ == '__main__':
     app.run()
 
